c590f37dfee6de98d15a681037d5e833675440da
momena <momena.salloum59@gmail.com>
2023-11-21 00:36:56 +0300

    Done with Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,24 @@
 import React from "react";
+import NavBar from "./NavBar";
+import Home from "./Home";
 
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
+{/* write an <About> component here */ }
+function About() {
+    return (
+        <div id="about" >
+            <h2>About</h2>
+        </div>
+    )
 }
-
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
-}
-
-{/* write an <About> component here */}
-
 
 function App() {
-  return (
-    <div>
-      <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
-    </div>
-  );
+    return (
+        <div>
+            <NavBar />
+            <Home />
+            <About />
+        </div>
+    );
 }
 
 export default App;
